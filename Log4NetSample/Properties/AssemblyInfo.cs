0e82112d41bbc1946589bf7dc968559a15cd19c6
tim <jan-brenner@gmx.de>
2017-03-29 15:18:09 +0200

    Initial commit.

---
+++ Log4NetSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+
+[assembly: AssemblyTitle("Log4NetSample")]
+[assembly: AssemblyDescription("Log4NetSample")]
+[assembly: AssemblyConfiguration("Log4NetSample")]
+[assembly: AssemblyCompany("Hämmer Electronics")]
+[assembly: AssemblyProduct("Log4NetSample")]
+[assembly: AssemblyCopyright("Copyright © Hämmer Electronics")]
+[assembly: AssemblyTrademark("Log4NetSample")]
+[assembly: AssemblyCulture("")]
+[assembly: log4net.Config.XmlConfigurator]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+
+[assembly: Guid("d8c78605-07ee-4eb6-b49f-f4c0eda4d8c3")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,11 @@
+#Log4NetSample
+===============
+Log4NetSample is an example on how to use log4net to write to a log file.
+The example was written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/5yf0u1kqh82n4p84?svg=true)](https://ci.appveyor.com/project/SeppPenner/log4netsample)
+
+Change history
+--------------
+
+* **Version 1.0.0.0 (2017-03-29)** : 1.0 release.
